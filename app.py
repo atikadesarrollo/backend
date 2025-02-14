455a2e28326364e1bd4c70ac1f020f14d1148e01
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2025-02-14 01:59:07 -0300

    analisis_proyecto

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+
+
+""" Productivo
+
 from flask import Flask
 from odoo_api.routes import odoo_bp
 from datalake_api.routes import datalake_bp
@@ -16,4 +20,25 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    serve(app, host='127.0.0.1', port=5000)
+    serve(app, host='0.0.0.0', port=5000)"""
+    
+# Desarrollo
+
+from flask import Flask
+from odoo_api.routes import odoo_bp
+from datalake_api.routes import datalake_bp
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # Habilitar CORS para toda la aplicación
+
+    # Registrar blueprints
+    app.register_blueprint(odoo_bp, url_prefix='/odoo')
+    app.register_blueprint(datalake_bp, url_prefix='/datalake')
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
