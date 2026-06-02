58f23730a4d4e3cd6aa726f95cafccf9a91b0eda
Alexis <a.gonzalez@atika.cl>
2026-06-02 15:16:19 -0400

    Update routes.py
    Reemplazar la vista de ventas y facruracion por su version en tabla.

--- datalake_api/routes.py
+++ datalake_api/routes.py
@@ -233,7 +233,7 @@ def get_analisis_venta_fechas(start_date, end_date):
                 ,[Email Esp Inmobiliario] as 'Email_esp_In'
                 ,[Valor Flete MO VE] as 'Valor_Flete_MO_VE'
                 ,[Origen]
-                FROM DL_Analisis_Venta_v 
+                FROM DL_Analisis_Venta 
                 WHERE CAST([Fecha de oferta] AS DATE) BETWEEN CAST(? AS DATE) AND CAST(? AS DATE)
                 AND [RUT Cliente] != '1-9'
                 --ordenar por fecha de oferta mayor a menor
@@ -442,7 +442,7 @@ def get_facturacion_fechas(start_date, end_date):
                     ,[Email Esp Inmobiliario] as 'Email_esp_In'
                     ,[Valor Flete MO VE] as 'Valor_Flete_MO_VE'
                     ,[Origen]
-                FROM DL_Facturacion_v 
+                FROM DL_Facturacion 
                 WHERE CAST([Fecha documento] AS DATE) BETWEEN CAST(? AS DATE) AND CAST(? AS DATE)
                 ORDER BY [Fecha documento] DESC;
             """
